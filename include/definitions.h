dd3977145f27bf3d41351024b1dada3c138450f6
Jessy Williams <jessy@jessywilliams.com>
2019-03-08 02:30:49 -0500

    Documents things

--- include/definitions.h
+++ include/definitions.h
@@ -1,15 +1,17 @@
 #pragma once
 
-
 // Constant Definitions --------------------------------------------------------
 
+// Configurable
+#define GAME_SPEED 2         //  1..10
+#define WORLD_XZ 100         // 50..150
+#define WORLD_Y 50           // 50..150
+#define LANDER_VISIBILITY 2  //  1..15
+
+// Not so much...
 #define LASER_DIST (WORLD_XZ * 1.5f)
+#define MAP_CLEAR 5
 #define MAX_CUBES 500000
 #define PGM_MAX_DIGITS 10
 #define PGM_MAX_DIM 1000
 #define PI 3.14159265358979323846f
-#define WORLD_XZ 100
-#define WORLD_Y 50
-#define LANDER_VISIBILITY 2
-#define GAME_SPEED 2
-#define MAP_CLEAR 5
